4631f37f230da2cd7feb93e925aec00ab5c186a2
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-09-05 08:54:03 +0530

    added changes

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,20 +1,12 @@
 import React, { ReactElement } from 'react';
 import Head from 'next/head';
 import Layout from '@common/Layout';
-import TransactionForm from 'Components/pages_componets/accounting/form';
-import Membershiptable from 'Components/pages_componets/membership/membershiptbale';
-import AccountingTable from 'Components/pages_componets/accounting/accountingtable';
-import User from 'Components/pages_componets/user/user';
-
 const index = () => {
 	return (
 		<React.Fragment>
 			<Head>
 				<title>User | Vidhyaroute -Admin </title>
 			</Head>
-			<div className='page-content'>
-				<User />
-			</div>
 		</React.Fragment>
 	);
 };
