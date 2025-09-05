d77b9fd6a4cfe3dd6f5084fc5d2f38372dac83a1
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-09-05 08:46:40 +0530

    added changes

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement } from 'react';
 import Head from 'next/head';
 import Layout from '@common/Layout';
-import MapComponent from 'Components/pages_componets/location/maps';
 const Dashboard = () => {
 	return (
 		<React.Fragment>
